120a44e9bffd5e02a82e1cac5c47dc19fb47bc7d
BADR KACIMI <31140256+BADRKAC@users.noreply.github.com>
2022-03-07 17:07:13 +0100

    Update Batching.js

--- Batching.js
+++ Batching.js
@@ -1,1 +1,18 @@
+import React, { useEffect, useState } from "react";
 
+function App() {
+  const [seconds, setSeconds] = useState(startSeconds);
+  const [minutes, setMinutes] = useState(startMiniutes);
+  const [hours, setHours] = useState(startHours);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSeconds(seconds + 1);
+      setMinutes(calcMinutes(seconds));
+      setHours(calcHours(seconds));
+    }, 1000);
+  });
+  return <ClockDisplay hours={hours} minutes={minutes} seconds={seconds} />;
+}
+
+//One Render
